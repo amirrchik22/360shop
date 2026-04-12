5212bcccbf08baff38de97883bb34d9271471a6f
amirrchik22 <amirrchik22@gmail.com>
2026-04-12 19:46:50 +0300

    Add files via upload

---
+++ config.js
@@ -0,0 +1,11 @@
+﻿window.SHEET_CONFIG = {
+  // Вставьте ID таблицы из URL:
+  // https://docs.google.com/spreadsheets/d/<SHEET_ID>/edit
+  sheetId: "195R58r71VxUuDyv606PHX2BDfSjSthOKnkz3ou6fXNw",
+
+  // Название вкладки (листа) в таблице.
+  sheetName: "Лист1",
+
+  // Частота автообновления страницы в миллисекундах.
+  refreshIntervalMs: 45000,
+};
